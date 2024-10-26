1f09abb0b3d98d19812b8db955b5d5a9bf3544bd
Ivan Romero <i.wells.ar@gmail.com>
2024-10-25 18:29:40 -0600

    account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,21 +1,63 @@
-import { getServerSession } from "next-auth";
+import { getServerAuthSession } from "rbrgs/server/auth";
+import { signOut } from "next-auth/react";
+import LoginText from "../_components/login-text";
+import { UserRole } from "rbrgs/util/UserRole";
+import { api } from "rbrgs/trpc/server";
+import { Separator } from "r/components/ui/separator";
 
 export default async function AccountPage() {
-  const session = await getServerSession();
-
+  const session = await getServerAuthSession();
   if (!session) {
     return (
-      <div>
-        <h1>Log in</h1>
+      <div className="mt-[4rem] rounded-md bg-gradient-to-r from-blue-rbrgs to-black p-10 text-white">
+        <LoginText />
         <p>You need to log in to access this page.</p>
       </div>
     );
   }
 
+  const data = await api.team.getTeam();
   return (
-    <div>
+    <div className="mt-[4rem] h-max bg-black p-10 text-white">
       <h1>Account</h1>
-      <p>You are logged in as {session.user.email}.</p>
+
+      <div className="m-4 rounded-md bg-gradient-to-r from-blue-rbrgs to-black p-10 text-white">
+        <p>
+          You are logged in as {session.user.email} - {session.user.name}
+        </p>
+        {session.user.role === UserRole.CONTESTANT && (
+          <p>
+            You can view the scoreboard, check your rounds&apos; times and
+            submit all of your teams documents.
+            <p className="font-bold">Give it your best!</p>
+          </p>
+        )}
+
+        {session.user.role === UserRole.JUDGE && (
+          <p>
+            You can view the scoreboard, check the rounds&apos; times and submit
+            your scores for each team.
+            <p className="font-bold">Good luck!</p>
+          </p>
+        )}
+
+        {session.user.role === UserRole.ADMIN && <p>ADMIN</p>}
+      </div>
+
+      {session.user.role === UserRole.CONTESTANT && (
+        <>
+          <h1>Team </h1>
+
+          <div className="m-4 rounded-md bg-gradient-to-r from-blue-rbrgs to-black p-10 text-white">
+            <h2 className="font-bold">Your Team {data?.name}:</h2>
+            {data?.members.map((member, key) => (
+              <div key={key}>
+                <p>{member.name}</p>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }

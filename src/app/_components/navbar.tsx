dcba51a0339810c932e0b9d55469d6167e266738
Ivan Romero <i.wells.ar@gmail.com>
2024-10-28 11:26:36 -0600

    link

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -2,21 +2,28 @@ import Image from "next/image";
 import robologo from "../../../public/images/white-logo.png";
 import SignInButton from "./SignInButton";
 import { getServerAuthSession } from "rbrgs/server/auth";
+import Link from "next/link";
 
 export default async function Navbar() {
   const session = await getServerAuthSession();
   return (
-    <nav className="w-screen h-[3rem] z-50 bg-black fixed top-0 grid grid-cols-3 items-center px-[3rem] font-archivo">
-        <Image src={robologo} alt="Logo" className="h-[2rem] w-fit object-contain cursor-pointer" />
-        <div className="flex items-center justify-evenly h-full text-white">
-            <p className="cursor-pointer">Scoreboard</p>
-            <p className="cursor-pointer">Dashboard</p>
-            <p className="cursor-pointer">Documents</p>
-            <p className="cursor-pointer">Team</p>
-        </div>
-        <div className="flex justify-end items-center">
-          <SignInButton session={session} />
-        </div>
+    <nav className="fixed top-0 z-50 grid h-[3rem] w-screen grid-cols-3 items-center bg-black px-[3rem] font-archivo">
+      <Link href="/" className="flex items-center align-middle">
+        <Image
+          src={robologo}
+          alt="Logo"
+          className="h-[2rem] w-fit cursor-pointer object-contain"
+        />
+      </Link>
+      <div className="flex h-full items-center justify-evenly text-white">
+        <p className="cursor-pointer">Scoreboard</p>
+        <p className="cursor-pointer">Dashboard</p>
+        <p className="cursor-pointer">Documents</p>
+        <p className="cursor-pointer">Team</p>
+      </div>
+      <div className="flex items-center justify-end">
+        <SignInButton session={session} />
+      </div>
     </nav>
   );
 }

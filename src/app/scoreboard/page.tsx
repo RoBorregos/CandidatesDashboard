c454c824f4ac6c5f245a16a86ef9ffe69cbcedc0
Gilberto Malagamba Montejo <A00839075@tec.mx>
2024-11-01 02:12:49 -0600

    fix 1.0

--- src/app/scoreboard/page.tsx
+++ src/app/scoreboard/page.tsx
@@ -10,7 +10,7 @@ import { Round } from "../../lib/round";
 const TwitchEmbed = ({ channel }: { channel: string }) => (
   <div className="aspect-video w-full">
     <iframe
-      src={`https://player.twitch.tv/?channel=${channel}&parent=www.candidates.roborregos.com&parent=localhost`}
+      src={`https://player.twitch.tv/?channel=${channel}&parent=none`}
       className="h-full w-full"
       allowFullScreen
     />

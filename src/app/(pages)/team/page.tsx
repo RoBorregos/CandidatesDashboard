49448f19472adbf017f24820e4afbb03d0dc4616
Gilberto Malagamba Montejo <148925854+GilMM27@users.noreply.github.com>
2025-10-03 15:12:18 -0600

    added feature (#33)

--- src/app/(pages)/team/page.tsx
+++ src/app/(pages)/team/page.tsx
@@ -5,18 +5,40 @@ import { getServerAuthSession } from "rbrgs/server/auth";
 import CustomLoginText from "../../_components/custom-login-text";
 import TeamInfo from "../../_components/team/team";
 
-export default async function TeamPage({ params }: { params: { teampage: string } }) {
+export default async function TeamPage() {
   const session = await getServerAuthSession();
 
   if (!session) {
     return (
-      <div className="h-screen flex items-center justify-center">
-        <CustomLoginText text="Please login to view your team information" label={"Login"} />
+      <div className="flex h-screen items-center justify-center">
+        <CustomLoginText
+          text="Please login to view your team information"
+          label={"Login"}
+        />
       </div>
     );
   }
+
   const team = await api.team.getTeam();
-  const rounds = team?.rounds;
+  if (!team) {
+    return (
+      <div className="mt-[4rem] h-96 bg-black text-sm text-white md:text-base">
+        <div className="md:pb-20">
+          <Header title="Team" subtitle="" />
+        </div>
+        <div className="flex h-96 flex-col items-center justify-center gap-4">
+          <h1 className="text-2xl font-bold text-white">
+            You are not assigned to a team yet.
+          </h1>
+          <p className="text-center text-white">
+            Please contact your mentors or website administrators if you think
+            this is a mistake.
+          </p>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 
   return (
     <div className="mt-[4rem] h-96 bg-black text-sm text-white md:text-base">
@@ -26,19 +48,25 @@ export default async function TeamPage({ params }: { params: { teampage: string 
 
       <div className="px-20 pb-20 pt-10">
         <h1 className="text-xl font-semibold">
-          <span className="font-normal text-gray-200 pr-1">&#60; </span> Welcome <span className="font-jersey_25 text-blue-700 text-3xl">{session.user.name}</span> ! <span className="font-normal pl-1 text-gray-200">/&#62;</span>
+          <span className="pr-1 font-normal text-gray-200">&#60; </span> Welcome{" "}
+          <span className="font-jersey_25 text-3xl text-blue-700">
+            {session.user.name}
+          </span>{" "}
+          ! <span className="pl-1 font-normal text-gray-200">/&#62;</span>
         </h1>
         <div>
-          Here you will find the schedules for your rounds and interviews as well as results for each round.
-          <br />Please make sure to be on time for rounds and interviews.
-          <br />Don&apos;t forget to add the link to your documents down below.
-          <br />A link to a google drive folder is fine, but remember we can see the last time it was updated/created. (Ensure permissions are correct for us to access the docs)
+          Here you will find the schedules for your rounds and interviews as
+          well as results for each round.
+          <br />
+          Please make sure to be on time for rounds and interviews.
+          <br />
+          Don&apos;t forget to add the link to your documents down below.
+          <br />A link to a google drive folder is fine, but remember we can see
+          the last time it was updated/created. (Ensure permissions are correct
+          for us to access the docs)
         </div>
-
       </div>
-
-      {team && <TeamInfo team={team} />}
-
+      <TeamInfo team={team} />
       <Footer />
     </div>
   );

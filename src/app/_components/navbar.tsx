be99cf00f1a7070d6adfec89a083f89590ce5a91
luiss_benvenuto <luiss.benvenuto@gmail.com>
2025-10-05 02:24:56 -0600

    Added join team to navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -25,11 +25,14 @@ export default async function Navbar() {
         <Link href="/">Home</Link>
         <Link href="scoreboard">Scoreboard</Link>
         <Link href="team">Team</Link>
+        <Link href="request">Join a team</Link>
         {canJudge && <Link href="judge">Judge</Link>}
         {session?.user.role === Role.ADMIN && <Link href="admin">Admin</Link>}
         <Link href="https://www.roborregos.com">About us</Link>
+        
       </div>
       <div className="hidden lg:block">
+
         <SignInButton session={session} />
       </div>
       <NavDropdown session={session} />

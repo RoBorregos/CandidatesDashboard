bc0fa5a8f86a8a9b8f841ec1fa54417d305990ae
luiss_benvenuto <luiss.benvenuto@gmail.com>
2025-10-16 23:48:56 -0600

    Change display only hour and minute for the interview

--- src/app/_components/admin/InterviewManagement.tsx
+++ src/app/_components/admin/InterviewManagement.tsx
@@ -149,9 +149,6 @@ export default function InterviewManagement({
 
   const formatDateTime = (date: Date) => {
     return date.toLocaleString("en-US", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
       hour: "2-digit",
       minute: "2-digit",
     });

f6c5d164034368dc0669b579eb765aba90ec5d40
Fernando94654 <A00841856@tec.mx>
2025-10-18 03:07:05 -0600

    update interviewer page

--- src/server/api/routers/interviewer.ts
+++ src/server/api/routers/interviewer.ts
@@ -38,4 +38,63 @@ export const interviewerRouter = createTRPCRouter({
     if (!interviewer) return false;
     return true;
   }),
+  isStaff: protectedProcedure.query(async ({ ctx }) => {
+    const user = await ctx.db.user.findUnique({
+      where: { id: ctx.session.user?.id },
+    });
+
+    if (!user?.isStaff) return false;
+    return true;
+  }),
+  getMecanicsInterviewers: protectedProcedure.query(async ({ ctx }) => {
+    const interviewers = await ctx.db.user.findMany({
+      where: { interviewer: { area: "MECHANICS" } },
+      include: {
+        interviewer: {
+          select: { id: true, name: true, area: true, email: true },
+        },
+        team: {
+          include: {
+            rounds: { include: { challenges: true } },
+          },
+        },
+      },
+      orderBy: [{ interviewTime: "asc" }, { name: "asc" }],
+    });
+    return interviewers;
+  }),
+  getElectronicsInterviewers: protectedProcedure.query(async ({ ctx }) => {
+    const interviewers = await ctx.db.user.findMany({
+      where: { interviewer: { area: "ELECTRONICS" } },
+      include: {
+        interviewer: {
+          select: { id: true, name: true, area: true, email: true },
+        },
+        team: {
+          include: {
+            rounds: { include: { challenges: true } },
+          },
+        },
+      },
+      orderBy: [{ interviewTime: "asc" }, { name: "asc" }],
+    });
+    return interviewers;
+  }),
+  getProgrammingInterviewers: protectedProcedure.query(async ({ ctx }) => {
+    const interviewers = await ctx.db.user.findMany({
+      where: { interviewer: { area: "PROGRAMMING" } },
+      include: {
+        interviewer: {
+          select: { id: true, name: true, area: true, email: true },
+        },
+        team: {
+          include: {
+            rounds: { include: { challenges: true } },
+          },
+        },
+      },
+      orderBy: [{ interviewTime: "asc" }, { name: "asc" }],
+    });
+    return interviewers;
+  }),
 });

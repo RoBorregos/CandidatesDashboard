07bc67745ad4665729b8475d9472ef8f2a75c276
luiss_benvenuto <luiss.benvenuto@gmail.com>
2025-10-17 19:48:51 -0600

    Added option to upload robot image

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -5,50 +5,27 @@ import { db } from "./db";
 
 const f = createUploadthing();
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({
-    image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
-      maxFileSize: "4MB",
-      maxFileCount: 1,
-    },
+    image: { maxFileSize: "4MB", maxFileCount: 1 },
   })
-    // Set permissions and file types for this FileRoute
     .middleware(async () => {
-      // This code runs on your server before upload
       const session = await getServerAuthSession();
-
-      // If you throw, the user will not be able to upload
-      // eslint-disable-next-line @typescript-eslint/only-throw-error
       if (!session) throw new UploadThingError("Unauthorized");
-
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: session.user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
-
       console.log("file url", file.ufsUrl);
-
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
     }),
+
   binnacleUploader: f({
-    pdf: {
-      maxFileSize: "4MB",
-      maxFileCount: 1,
-    },
+    pdf: { maxFileSize: "4MB", maxFileCount: 1 },
   })
     .middleware(async () => {
       const session = await getServerAuthSession();
       if (!session?.user?.teamId)
-        // eslint-disable-next-line @typescript-eslint/only-throw-error
         throw new UploadThingError("User isn't in a team");
       return { teamId: session.user.teamId };
     })
@@ -57,9 +34,29 @@ export const ourFileRouter = {
         where: { id: metadata.teamId },
         data: { binnacleLink: file.ufsUrl },
       });
-
-      // Return the updated data for client callback
       return { binnacleLink: file.ufsUrl, teamId: metadata.teamId };
+    }),
+
+  robotImageUploader: f({
+    "image/jpeg": { maxFileSize: "4MB", maxFileCount: 1 },
+    "image/png": { maxFileSize: "4MB", maxFileCount: 1 },
+  })
+    .middleware(async () => {
+      const session = await getServerAuthSession();
+      if (!session?.user?.teamId)
+        throw new UploadThingError("User isn't in a team");
+      return { teamId: session.user.teamId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // Extra security check
+      if (!["image/jpeg", "image/png"].includes(file.type)) {
+        throw new UploadThingError("Only PNG or JPEG images are allowed");
+      }
+      await db.team.update({
+        where: { id: metadata.teamId },
+        data: { robotImageLink: file.ufsUrl },
+      });
+      return { robotImageLink: file.ufsUrl, teamId: metadata.teamId };
     }),
 } satisfies FileRouter;
 

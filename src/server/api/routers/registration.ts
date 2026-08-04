66083a5051b77d42f537bf1fd5f5b046a629f79c
camila_tite <a00841255@tec.mx>
2026-08-04 13:16:41 -0500

    fix: remove unused import of Role from registration router

--- src/server/api/routers/registration.ts
+++ src/server/api/routers/registration.ts
@@ -1,10 +1,6 @@
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
-<<<<<<< HEAD
 import { RegistrationStatus, Role } from "@prisma/client";
-=======
-import { RegistrationStatus } from "@prisma/client";
->>>>>>> 6033ba2754dd78324b4c966359731efad015338d
 
 import {
   adminProcedure,
